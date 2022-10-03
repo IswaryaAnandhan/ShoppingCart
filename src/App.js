ddbb0345b27036167e32cafcdc7e0f7206b11365
IswaryaAnandhan <aishwariyaanandhan@gmail.com>
2022-10-03 20:04:19 +0530

    added quantity to cart

--- src/App.js
+++ src/App.js
@@ -9,54 +9,63 @@ function App() {
     { id: 1,
       name: "Iphone 14",
       Price: 79900,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662703724/Croma%20Assets/Communication/Mobiles/Images/261934_qgssvy.png/mxw_2048,s_webp,f_auto"
     },
     {
       id: 2,
       name: "Samsung S22",
       Price: 66990,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662442140/Croma%20Assets/Communication/Mobiles/Images/248899_btsarx.png/mxw_2048,s_webp,f_auto"
     },
     {
       id: 3,
       name: "Redmi",
       Price: 13000,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662438345/Croma%20Assets/Communication/Mobiles/Images/251043_usggge.png/mxw_2048,s_webp,f_auto"
     },
     {
       id: 4,
       name: "Oneplus",
       Price: 36000,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662415157/Croma%20Assets/Communication/Mobiles/Images/233905_z8fbao.png/mxw_2048,s_webp,f_auto"
     },
     { 
       id: 5,
       name: "Oppo",
       Price: 30000,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662436748/Croma%20Assets/Communication/Mobiles/Images/251268_nll6my.png/mxw_2048,s_webp,f_auto"
     },
     {
       id: 6,
       name: "Nokia",
       Price: 28000,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662415632/Croma%20Assets/Communication/Mobiles/Images/233802_dwrnaw.png/mxw_2048,s_webp,f_auto"
     },
     {
       id: 7,
       name: "VIVO",
       Price: 33000,
+      quantity:1,
       img:"https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662422853/Croma%20Assets/Communication/Mobiles/Images/242178_fthzs6.png/mxw_2048,s_webp,f_auto"
     },
     {
       id: 8,
       name: "POCO",
       Price: 14000,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662444310/Croma%20Assets/Communication/Mobiles/Images/250889_b0qyb3.png/mxw_2048,s_webp,f_auto"
     },
     {
       id: 9,
       name: "Realme",
       Price: 18990,
+      quantity:1,
       img: "https://d2d22nphq0yz8t.cloudfront.net/88e6cc4b-eaa1-4053-af65-563d88ba8b26/https://media.croma.com/image/upload/v1662437111/Croma%20Assets/Communication/Mobiles/Images/251592_yss7ar.png/mxw_2048,s_webp,f_auto"
     },
   ]) 
@@ -92,7 +101,7 @@ function App() {
         </div>
         <div className='col-lg-4'>
          <h3>Cart</h3>
-         <Cart cartItems={cartItems} removeFromCart={removeFromCart}/>
+         <Cart cartItems={cartItems} removeFromCart={removeFromCart} setCartItems={setCartItems}/>
         </div>
       </div>
     </div>
--- src/Cart.js
+++ src/Cart.js
@@ -1,5 +1,21 @@
 import React from "react";
-function Cart({ cartItems, removeFromCart}) {
+
+function Cart({removeFromCart,cartItems,setCartItems}) {
+  const handleDecrement = (card_id) => {
+    setCartItems(cartItems =>
+      cartItems.map((item)=>
+        card_id === item.id ? {...item,quantity:item.quantity - (item.quantity > 1 ? 1 : 0)} : item
+      )
+      );
+  }
+  const handleIncrement= (card_id)=>{
+    setCartItems(cartItems =>
+      cartItems.map((item)=>
+        card_id === item.id ? {...item,quantity:item.quantity + (item.quantity < 5 ? 1 : 0)} : item
+      )
+      );
+  }
+
   return (
     <>
       {
@@ -13,7 +29,15 @@ function Cart({ cartItems, removeFromCart}) {
                 <li className="list-group-item d-flex justify-content-between align-items-start">
                   <div className="ms-2 me-auto">
                     <div className="fw-bold">{items.name}</div>
-                    Rs.{items.Price}
+                    Rs.{items.Price*items.quantity}
+                  </div>
+                  <div className="ms-2 me-auto">
+             <div className="input-group ms-2">
+             <button onClick={()=>handleDecrement(items.id)} className="input-group-text" >-</button>
+         
+             <div className="form-control text-center">{items.quantity}</div>
+          <button onClick={()=>handleIncrement(items.id)}  className="input-group-text" >+</button>
+           </div>
                   </div>
                   <button
                     onClick={() => {
@@ -30,7 +54,7 @@ function Cart({ cartItems, removeFromCart}) {
           <h3>
             Total: Rs.
             {cartItems.reduce((prev, curr) => {
-              return (prev = prev + curr.Price);
+              return (prev = prev + curr.Price*curr.quantity);
             }, 0)}
           </h3>
         </>
